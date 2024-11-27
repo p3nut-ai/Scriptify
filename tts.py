c431c36f760f81a8c84eed1dd3de5d2c56d01394
p3nut-ai <jaquiasjonathan2@gmail.com>
2024-11-27 23:42:15 +0800

    remove voices make it shorter

--- tts.py
+++ tts.py
@@ -13,19 +13,10 @@ lock = Lock()
 
 VOICES = {
     "en_us_ghostface": "Ghost Face",
-    "en_au_001": "English AU - Female",
     "en_au_002": "English AU - Male",
     "en_uk_001": "English UK - Male 1",
-    "en_uk_003": "English UK - Male 2",
-    "en_us_001": "English US - Female (Int. 1)",
-    "en_us_002": "English US - Female (Int. 2)",
-    "en_us_006": "English US - Male 1",
-    "en_us_007": "English US - Male 2",
-    "en_us_009": "English US - Male 3",
     "en_us_010": "English US - Male 4",
     "en_male_narration": "English Male Narrator",
-    "en_male_funny": "English Male Funny",
-    "en_female_emotional": "English Female Emotional"
 }
 
 ENDPOINTS = [
